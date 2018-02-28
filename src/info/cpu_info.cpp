3381cb5adb34af5b0eeaa30c4dbd5a63a37807c5
Allee Zarrini <adzarrini@bb136-10.mines.edu>
2018-02-27 20:16:58 -0700

    Passing all tests for cpu_info

--- src/info/cpu_info.cpp
+++ src/info/cpu_info.cpp
@@ -8,7 +8,26 @@ using namespace std;
 
 vector<CpuInfo> get_cpu_info() {
   // TODO: implement me
-  return vector<CpuInfo>();
+	vector<CpuInfo> cpuInfo;
+
+	ifstream stat(PROC_ROOT "/stat");
+	if(!stat) {
+		cerr << "Error opening /proc/stat" << endl;
+		exit(EXIT_FAILURE);
+	}
+	string cpuFirst;
+	while (stat >> cpuFirst) {
+		CpuInfo cpu;
+		if(!cpuFirst.find("cpu")) {
+			stat >> cpu.user_time >> cpu.nice_time >> cpu.system_time >> cpu.idle_time
+			>> cpu.io_wait_time >> cpu.irq_time >> cpu.softirq_time >> cpu.steal_time
+			>> cpu.guest_time >> cpu.guest_nice_time;
+			cpuInfo.push_back(cpu);
+		}
+
+	}
+
+  return cpuInfo;
 }
 
 
