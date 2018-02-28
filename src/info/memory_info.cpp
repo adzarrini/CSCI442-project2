f72da6195082e70766080b2b8537da3b7364c6a6
Allee Zarrini <adzarrini@bb136-10.mines.edu>
2018-02-27 20:25:40 -0700

    Passing all tests but 1. Don't know why not passing. Will come back to.

--- src/info/memory_info.cpp
+++ src/info/memory_info.cpp
@@ -8,5 +8,30 @@ using namespace std;
 
 MemoryInfo get_memory_info() {
   // TODO: implement me
-  return MemoryInfo();
+
+	MemoryInfo memInfo;
+
+	ifstream meminfo (PROC_ROOT "/meminfo");
+	if(!meminfo) {
+		cerr << "Error opening /proc/meminfo" << endl;
+		exit(EXIT_FAILURE);
+	}
+	string memType;
+	while(meminfo >> memType) {
+		string unit;
+
+		if(memType == "MemTotal:") meminfo >> memInfo.total_memory;
+		if(memType == "MemFree:") meminfo >> memInfo.free_memory;
+		if(memType == "Buffers:") meminfo >> memInfo.buffers_memory;
+		if(memType == "Cached:") meminfo >> memInfo.cached_memory;
+		if(memType == "SwapTotal:") meminfo >> memInfo.total_swap;
+		if(memType == "SwapFree:") meminfo >> memInfo.free_swap;
+
+		meminfo >> unit;
+
+	}
+
+	meminfo.close();
+
+  return memInfo;
 }
