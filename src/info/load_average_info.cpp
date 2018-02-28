eb090f9ce085be8eefe87298b3bd209ce2179577
Allee Zarrini <adzarrini@bb136-10.mines.edu>
2018-02-27 19:57:38 -0700

    Passing all tests for load_average

--- src/info/load_average_info.cpp
+++ src/info/load_average_info.cpp
@@ -7,5 +7,17 @@ using namespace std;
 
 LoadAverageInfo get_load_average() {
   // TODO: implement me
-  return LoadAverageInfo();
+	LoadAverageInfo aveInfo;
+
+	ifstream loadavg (PROC_ROOT "/loadavg");
+	if(!loadavg) {
+		cerr << "Error opening /proc/loadavg" << endl;
+		exit(EXIT_FAILURE);
+	}
+	
+	loadavg >> aveInfo.one_min >> aveInfo.five_mins >> aveInfo.fifteen_mins;
+
+	loadavg.close();
+
+  return aveInfo;
 }
